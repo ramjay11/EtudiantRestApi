29a52150e9267b210906953d1e75c2f461d35f7a
Ramjay <ramjayromorosa@yahoo.com>
2023-12-24 22:38:02 +0800

    Add project files.

---
+++ EtudiantRestApi/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtudiantRestApi
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ EtudiantRestApi/Models/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EtudiantRestApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Etudiant> Etudiants { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Send Student Table
+            modelBuilder.Entity<Etudiant>().HasData(
+                new Etudiant
+                {
+                    EtudiantId = 1,
+                    FirstName = "Siber",
+                    LastName = "Saint",
+                    Email = "siber@yahoo.com",
+                    DateOfBirth = new DateTime(2001, 10, 1),
+                    Gender = Gender.Male,
+                    DepartmentId = 1,
+                    PhhotoPath = "Images/siber.png"
+                });
+            modelBuilder.Entity<Etudiant>().HasData(
+                new Etudiant
+                {
+                    EtudiantId = 2,
+                    FirstName = "Doray",
+                    LastName = "Saint",
+                    Email = "doray@yahoo.com",
+                    DateOfBirth = new DateTime(2002, 09, 2),
+                    Gender = Gender.Female,
+                    DepartmentId = 2,
+                    PhhotoPath = "Images/doray.png"
+                });
+
+        }  
+        
+    }
+}
